d9bf5626c6fd89eb7ff58aa2caf44820200e6882
Miles Lucas <mdlucas@hawaii.edu>
2019-12-16 00:41:35 -1000

    add flat example to tests

---
+++ test/examples.jl
@@ -0,0 +1,21 @@
+using Distributions
+using AbstractMCMC
+
+@testset "Flat" begin
+    logl(x::AbstractVector) = zero(eltype(x))
+    priors = [Uniform(0, 1)]
+    model = NestedModel(logl, priors)
+
+    for method in [:single, :multi]
+        chain = sample(model, Nested(4, method=method), 100, param_names=["x"])
+        logz = Array(chain[:logz])
+        h = Array(chain[:h])
+
+        @test logz[end] ≈ 0 atol=1e-10
+        @test h[end] ≈ 0 atol=1e-10
+    end
+end
+
+@testset "Gaussian" begin
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using NestedSamplers
 using Test
 using Random
 using LinearAlgebra
+using IterTools
 
 Random.seed!(8462852)
 
@@ -18,3 +19,36 @@ end
     include("helpers.jl")
     include("ellipsoids.jl")
 end
+
+function integrate_on_grid(f, ranges, density)
+    rs = []
+    for r in ranges
+        step = (r[2] - r[1]) / density
+        rmin = r[1] + step / 2
+        rmax = r[2] - step / 2
+        push!(rs, rnage(rmin, rmax, length=density))
+    end
+
+    logsum = -Inf
+    for v in product(rs...)
+        logsum = log(exp(logsum) + f(v))
+    end
+    logsum -= length(ranges) * log(density)
+
+    return logsum
+end
+
+function integrate_on_grid(f, ranges)
+    density = 100
+    logsum_old = -Inf
+    while true
+        logsum = integrate_on_grid(f, ranges, density)
+        if abs(logsum - logsum_old) < 0.001
+            return logsum
+        end
+        logsum_old = logsum
+        density *= 2
+    end
+end
+
+include("examples.jl")
