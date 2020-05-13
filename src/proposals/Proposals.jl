9c0bfae258c6c51717429fa2f16b8cadbf11be21
Miles Lucas <mdlucas@hawaii.edu>
2020-05-12 23:12:33 -1000

    add Uniform proposal

--- src/proposals/Proposals.jl
+++ src/proposals/Proposals.jl
@@ -1,5 +1,13 @@
 module Proposals
 
-abstract type AbstractProposal{T} end
+abstract type AbstractProposal end
+
+struct Uniform <: AbstractProposal end
+
+function (::Uniform)(u, prior_transform, loglike, args...; kwargs...)
+    v = prior_transform(u)
+    logl = loglike(v)
+    return u, v, logl
+end
 
 end # module Proposals
