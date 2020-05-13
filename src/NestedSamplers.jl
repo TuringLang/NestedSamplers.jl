edfa366c4226f5d64685538b8911709c436d807f
Miles Lucas <mdlucas@hawaii.edu>
2020-05-12 23:15:43 -1000

    fix load order

--- src/NestedSamplers.jl
+++ src/NestedSamplers.jl
@@ -1,31 +1,33 @@
 module NestedSamplers
 
-using LinearAlgebra
-using Random
-using Random: AbstractRNG, GLOBAL_RNG
-
-import AbstractMCMC: AbstractSampler,
-                     AbstractModel,
-                     step!,
-                     sample_init!,
-                     sample_end!,
-                     bundle_samples,
-                     mcmcsample
-using Distributions
-using MCMCChains: Chains
-import StatsBase
-using StatsFuns: logaddexp,
-                 log1mexp
-
+# load submodules
 include("bounds/Bounds.jl")
 include("proposals/Proposals.jl")
 
-export Bounds
+export Bounds,
+       Proposals
     #     NestedModel,
     #    Nested,
     #    dlogz_convergence,
     #    decline_convergence,
        
+using LinearAlgebra
+using Random
+using Random: AbstractRNG, GLOBAL_RNG
+
+import AbstractMCMC: AbstractSampler,
+                        AbstractModel,
+                        step!,
+                        sample_init!,
+                        sample_end!,
+                        bundle_samples,
+                        mcmcsample
+using Distributions
+using MCMCChains: Chains
+import StatsBase
+using StatsFuns: logaddexp,
+                    log1mexp
+
 
 # include("model.jl")         # The default model for nested sampling
 # include("staticsampler.jl") # The static nested sampler
