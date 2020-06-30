a3982ec9eb4ae382248864a15ae652c764ee51eb
Saranjeet Kaur <kaur.saranjeet3@gmail.com>
2020-07-01 00:28:10 +0530

    changing RStagger docstring
    Co-authored-by: Miles Lucas <mdlucas@hawaii.edu>

--- src/proposals/Proposals.jl
+++ src/proposals/Proposals.jl
@@ -147,7 +147,10 @@ This differs from the random walk proposal in that the step size here is exponen
 to reach a target acceptance rate _during_ each proposal, in addition to _between_
 proposals.
 
-`ratio` is the target acceptance ratio, `walks` is the minimum number of steps to take, and `scale` is the proposal distribution scale, which will update _between_ proposals.
+## Parameters
+- `ratio` is the target acceptance ratio
+- `walks` is the minimum number of steps to take
+- `scale` is the proposal distribution scale, which will update _between_ proposals.
 """
 @with_kw mutable struct RStagger <: AbstractProposal
     ratio = 0.5
