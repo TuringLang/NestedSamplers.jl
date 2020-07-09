034b75b834bfbdcdc434e70491fd69ba4edd9bb0
Saranjeet Kaur <kaur.saranjeet3@gmail.com>
2020-07-09 16:43:04 +0530

    `ndims` in test for `default_update_interval`
    For instance:
    @test default_update_interval(Proposals.Uniform(), ndims=3) == 1.5
     @test default_update_interval(Proposals.Slice(slices=10), ndims=25) == 225

--- test/sampler.jl
+++ test/sampler.jl
@@ -1,13 +1,13 @@
 using NestedSamplers: default_update_interval
 
 @testset "default helpers" begin
-    @test default_update_interval(Proposals.Uniform()) == 1.5
-    @test default_update_interval(Proposals.RWalk()) == 3.75
-    @test default_update_interval(Proposals.RWalk(walks=10)) == 1.5
-    @test default_update_interval(Proposals.RStagger()) == 3.75
-    @test default_update_interval(Proposals.RStagger(walks=10)) == 1.5
-    # @test default_update_interval(Proposals.Slice()) == 0.9 * ndims * 5   ## check what default to set for ndims?
-    # @test default_update_interval(Proposals.Slice(slices=10)) == 0.9 * ndims * 10  ## check what default to set for ndims?
+    @test default_update_interval(Proposals.Uniform(), ndims=3) == 1.5
+    @test default_update_interval(Proposals.RWalk(), ndims=10) == 3.75
+    @test default_update_interval(Proposals.RWalk(walks=10), ndims=10) == 1.5
+    @test default_update_interval(Proposals.RStagger(), ndims=10) == 3.75
+    @test default_update_interval(Proposals.RStagger(walks=10), ndims=10) == 1.5
+    @test default_update_interval(Proposals.Slice(), ndims=30) == 135  
+    @test default_update_interval(Proposals.Slice(slices=10), ndims=25) == 225
 end
 
 spl = Nested(3, 100)
@@ -32,6 +32,6 @@ spl = Nested(10, 1000)
 @test spl.proposal isa Proposals.RWalk
 @test spl.update_interval == 3750
 
-## spl = Nested(25, 1500)
-## @test spl.proposal isa Proposals.Slice
-## @test spl.update_interval ==    ## decide this once default for ndims is set!
+spl = Nested(30, 1500)
+@test spl.proposal isa Proposals.Slice
+@test spl.update_interval == 135
