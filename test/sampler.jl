3f405baf29e6f02766a04246771718b8b4378b47
Saranjeet Kaur <kaur.saranjeet3@gmail.com>
2020-07-15 11:20:42 +0530

    ndims & .RSlice in test
    test added for Proposals.RSlice (once .Slice is included remove .RSlice)

--- test/sampler.jl
+++ test/sampler.jl
@@ -1,11 +1,11 @@
 using NestedSamplers: default_update_interval
 
 @testset "default helpers" begin
-    @test default_update_interval(Proposals.Uniform()) == 1.5
-    @test default_update_interval(Proposals.RWalk()) == 3.75
-    @test default_update_interval(Proposals.RWalk(walks=10)) == 1.5
-    @test default_update_interval(Proposals.RStagger()) == 3.75
-    @test default_update_interval(Proposals.RStagger(walks=10)) == 1.5
+    @test default_update_interval(Proposals.Uniform(), ndims=3) == 1.5
+    @test default_update_interval(Proposals.RWalk(), ndims=10) == 3.75
+    @test default_update_interval(Proposals.RWalk(walks=10), ndims=10) == 1.5
+    @test default_update_interval(Proposals.RStagger(), ndims=10) == 3.75
+    @test default_update_interval(Proposals.RStagger(walks=10), ndims=10) == 1.5
     @test default_update_interval(Proposals.RSlice(), ndims=30) == 10  
     @test default_update_interval(Proposals.RSlice(slices=10), ndims=25) == 20
 end
@@ -31,3 +31,7 @@ Nested(ndims=3, nactive=100, enlarge=1.25, update_interval=150)
 spl = Nested(10, 1000)
 @test spl.proposal isa Proposals.RWalk
 @test spl.update_interval == 3750
+
+spl = Nested(30, 1500)
+@test spl.proposal isa Proposals.RSlice
+@test spl.update_interval == 10
