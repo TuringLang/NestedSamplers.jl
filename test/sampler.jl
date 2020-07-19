b682a53a65f7f33e0723b97bb08113fd4ea7576e
Miles Lucas <mdlucas@hawaii.edu>
2020-07-19 18:56:42 -0500

    fix sampler interface tests

--- test/sampler.jl
+++ test/sampler.jl
@@ -1,13 +1,13 @@
 using NestedSamplers: default_update_interval
 
 @testset "default helpers" begin
-    @test default_update_interval(Proposals.Uniform(), ndims=3) == 1.5
-    @test default_update_interval(Proposals.RWalk(), ndims=10) == 3.75
-    @test default_update_interval(Proposals.RWalk(walks=10), ndims=10) == 1.5
-    @test default_update_interval(Proposals.RStagger(), ndims=10) == 3.75
-    @test default_update_interval(Proposals.RStagger(walks=10), ndims=10) == 1.5
-    @test default_update_interval(Proposals.RSlice(), ndims=30) == 10  
-    @test default_update_interval(Proposals.RSlice(slices=10), ndims=25) == 20
+    @test default_update_interval(Proposals.Uniform(), 3) == 1.5
+    @test default_update_interval(Proposals.RWalk(), 10) == 3.75
+    @test default_update_interval(Proposals.RWalk(walks=10), 10) == 1.5
+    @test default_update_interval(Proposals.RStagger(), 10) == 3.75
+    @test default_update_interval(Proposals.RStagger(walks=10), 10) == 1.5
+    @test default_update_interval(Proposals.RSlice(), 30) == 10  
+    @test default_update_interval(Proposals.RSlice(slices=10), 25) == 20
 end
 
 spl = Nested(3, 100)
@@ -34,4 +34,5 @@ spl = Nested(10, 1000)
 
 spl = Nested(30, 1500)
 @test spl.proposal isa Proposals.RSlice
-@test spl.update_interval == 10
+@test spl.update_interval == 15000
+
