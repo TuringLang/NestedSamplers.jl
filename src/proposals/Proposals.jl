850b350d7fa9b3859e2ea17c0a9d499764573e02
Miles Lucas <mdlucas@hawaii.edu>
2020-07-19 18:57:06 -0500

    refactor slice_sampling and resolve RSlice tests

--- src/proposals/Proposals.jl
+++ src/proposals/Proposals.jl
@@ -38,98 +38,6 @@ abstract type AbstractProposal end
 
 # Helper for checking unit-space bounds
 unitcheck(us) = all(u -> 0 < u < 1, us)
-
-# Method for slice sampling
-function slicing(axis, u, logl_star, nc, nexpand, ncontract)
-    # define starting window
-    r = rand(rng)  # initial scale/offset
-    u_l = @. u - r * axis  # left bound
-    if unitcheck(u_l)
-        v_l = prior_transform(u_l)
-        logl_l = loglike(v_l)
-    else
-        logl_l = -Inf
-    end
-    nc += 1
-    nexpand += 1 
-
-    u_r = u_l .+ axis # right bound
-    if unitcheck(u_r)
-        v_r = prior_transform(u_r)
-        logl_r = loglike(v_r)
-    else
-        logl_r = -Inf
-    end    
-    nc += 1
-    nexpand += 1 
-
-    # stepping out left and right bounds
-    while logl_l >= logl_star
-        u_l .-= axis
-        if unitcheck(u_l)   
-            v_l = prior_transform(u_l)
-            logl_l = loglike(v_l)
-        else
-            logl_l = -Inf
-        end
-        nc += 1
-        nexpand += 1 
-    end
-
-    while logl_r >= logl_star
-        u_r .+= axis
-        if unitcheck(u_r)   
-            v_r = prior_transform(u_r)
-            logl_r = loglike(v_r)
-        else
-            logl_r = -Inf
-        end
-        nc += 1
-        nexpand += 1 
-    end
-
-    # sample within limits. If the sample is not valid, shrink the limits until the `logl_star` bound is hit
-    window_init = norm(u_r - u_l)  # initial window size
-    while true
-
-        # define slice and window
-        u_hat = u_r - u_l
-        window = norm(u_hat)
-
-        # check if the slice has shrunk to be ridiculously small
-        window < 1e-5 * window_init && error("Slice sampling appears to be stuck.")
-
-        # propose a new position
-        u_prop = @. u_l + rand(rng) * u_hat   # scale from left
-        if unitcheck(u_prop) 
-            v_prop = prior_transform(u_prop)
-            logl_prop = loglike(v_prop)
-        else
-            logl_prop = -Inf
-        end
-        nc += 1
-        ncontract += 1
-
-        # if success, then move to the new position
-        if logl_prop >= logl_star
-            u = u_prop
-            break                
-        # if fail, then check if the new point is to the left/right of the original point along the proposal axis and update the bounds accordingly
-        else
-            s = dot(u_prop - u, u_hat)       # check sign (+/-)
-            if s < 0   # left
-                u_l = u_prop
-            elseif s > 0  # right
-                u_r = u_prop
-            else # if `s = 0` something has gone wrong
-                error("Slice sampler has failed to find a valid point.")
-            end
-        end
-    end # end of sample within limits while    
-
-    return (nexpand, ncontract) 
-end    
-
 """
     Proposals.Uniform()
 
@@ -339,7 +247,7 @@ function (prop::RStagger)(rng::AbstractRNG,
             
     return u, v, logl, ncall
 end
-      
+
 """
     Proposals.RSlice(;slices=5, scale=1)
 Propose a new live point by a series of random slices away from an existing live point.
@@ -366,8 +274,7 @@ function (prop::RSlice)(rng::AbstractRNG,
     # setup
     n = length(point)
     nc = nexpand = ncontract = 0
-    local r, u, u_prop, v_prop, logl_prop, logl_l, logl_r, drhat      
-
+    local u, v, logl
     # random slice sampling loop
     for it in 1:prop.slices
     
@@ -376,17 +283,103 @@ function (prop::RSlice)(rng::AbstractRNG,
         drhat /= norm(drhat)
         
         # transform and scale into parameter space
-        axis = prop.scale .* (axes * drhat)
-        
-        slicing(axis, u, logl_star, nc, nexpand, ncontract)
-    
+        axis = prop.scale .* (Bounds.axes(bounds) * drhat)
+        u, v, logl, nc, nexpand, ncontract = sample_slice(rng, axis, point, logl_star, loglike,
+                                                          prior_transform, nc, nexpand, ncontract)
     end # end of random slice sampling loop
     
     # update random slice proposal scale based on the relative size of the slices compared to the initial guess... ## incomplete (check formula for this step)
     prop.scale = prop.scale * nexpand / (2.0 * ncontract)
     
-    return u_prop, v_prop, logl_prop, nc
+    return u, v, logl, nc
 end # end of function RSlice
-    
+
+# Method for slice sampling
+function sample_slice(rng, axis, u, logl_star, loglike, prior_transform, nc, nexpand, ncontract) 
+    # define starting window
+    r = rand(rng)  # initial scale/offset
+    u_l = @. u - r * axis  # left bound
+    if unitcheck(u_l)
+        v_l = prior_transform(u_l)
+        logl_l = loglike(v_l)
+    else
+        logl_l = -Inf
+    end
+    nc += 1
+    nexpand += 1 
+
+    u_r = u_l .+ axis # right bound
+    if unitcheck(u_r)
+        v_r = prior_transform(u_r)
+        logl_r = loglike(v_r)
+    else
+        logl_r = -Inf
+    end    
+    nc += 1
+    nexpand += 1 
+
+    # stepping out left and right bounds
+    while logl_l ≥ logl_star
+        u_l .-= axis
+        if unitcheck(u_l)   
+            v_l = prior_transform(u_l)
+            logl_l = loglike(v_l)
+        else
+            logl_l = -Inf
+        end
+        nc += 1
+        nexpand += 1 
+    end
+
+    while logl_r ≥ logl_star
+        u_r .+= axis
+        if unitcheck(u_r)   
+            v_r = prior_transform(u_r)
+            logl_r = loglike(v_r)
+        else
+            logl_r = -Inf
+        end
+        nc += 1
+        nexpand += 1 
+    end
+
+    # sample within limits. If the sample is not valid, shrink the limits until the `logl_star` bound is hit
+    window_init = norm(u_r - u_l)  # initial window size
+    while true 
+        # define slice and window
+        u_hat = u_r - u_l
+        window = norm(u_hat)
+
+        # check if the slice has shrunk to be ridiculously small
+        window < 1e-5 * window_init && error("Slice sampling appears to be stuck.")
+
+        # propose a new position
+        r = rand(rng)
+        u_prop = @. u_l + r * u_hat
+        if unitcheck(u_prop) 
+            v_prop = prior_transform(u_prop)
+            logl_prop = loglike(v_prop)
+        else
+            logl_prop = -Inf
+        end
+        nc += 1
+        ncontract += 1
+
+        # if success, then move to the new position
+        if logl_prop ≥ logl_star
+            return u_prop, v_prop, logl_prop, nc, nexpand, ncontract
+        # if fail, then check if the new point is to the left/right of the original point along the proposal axis and update the bounds accordingly
+        else
+            s = dot(u_prop - u, u_hat)       # check sign (+/-)
+            if s < 0   # left
+                u_l = u_prop
+            elseif s > 0  # right
+                u_r = u_prop
+            else # if `s = 0` something has gone wrong
+                error("Slice sampler has failed to find a valid point.")
+            end
+        end
+    end # end of sample within limits while    
+end    
     
 end # module Proposals
