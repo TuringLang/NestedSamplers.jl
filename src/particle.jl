5ba18cf32e66a3dc453a8ea2653f454e42f6dcaa
Miles Lucas <mdlucas@hawaii.edu>
2020-12-13 20:36:53 -0600

    still not sure how to use Particles

--- src/particle.jl
+++ src/particle.jl
@@ -3,18 +3,7 @@ import StatsBase: ProbabilityWeights, pweights, Weights, weights
 """
     Particle(sample=missing, weight=missing, iteration=nothing)
 
-A particle type containing pertinent information from the sampling routine. `weight` is the statistical weight of the sample. 
-
-# Implements
-The following functions for retrieving the samples from a vector of particles are implemented:
-* `NestedSamplers.samples`
-
-The following functions for retrieving the statistical weights of a vector of particles are
-* `NestedSamplers.weights`
-* `StatsBase.ProbabilityWeights`
-* `StatsBase.pweights`
-* `StatsBase.Weights` 
-* `StatsBase.weights`
+A particle type containing pertinent information from the sampling routine. `weight` is the statistical weight of the sample.
 """
 struct Particle{T<:Union{Number,Missing},F<:Union{AbstractFloat,Missing}}
     sample::T
@@ -23,24 +12,3 @@ struct Particle{T<:Union{Number,Missing},F<:Union{AbstractFloat,Missing}}
 end
 Particle(sample, weight=missing, iteration=nothing) = Particle(sample, weight, iteration)
 Particle() = Particle(missing, missing, nothing)
-
-"""
-    NestedSamplers.samples(particles)
-
-Return the sample from each particle in the collection
-"""
-samples(particles) = map(p -> p.sample, particles)
-
-"""
-    NestedSamplers.weights(particles)
-
-Return the sampling weight for each particle's sample in the collection
-"""
-weights(particles) = map(p -> p.weight, particles)
-
-# StatsBase weights
-for func in (:ProbabilityWeights, :pweights, :Weights, :weights)
-    @eval begin
-        $func(particles, args...) = $func(weights(collect(particles)), args...)
-    end
-end
