e7f9a92ac2f9f59e37bc6ca178468951065e7c2c
Miles Lucas <mdlucas@hawaii.edu>
2021-04-11 12:20:28 -0500

    update sample interface to use AbstractMCMC 3.0.2

--- src/sample.jl
+++ src/sample.jl
@@ -1,61 +1,14 @@
-# Interface Implementations for running full sampling loops
+###
+### Interface Implementations for running full sampling loops
+###
+
 using Printf
 
 StatsBase.sample(rng::AbstractRNG, model, sampler::Nested; kwargs...) =
-    mcmcsample(rng, model, sampler; kwargs...)
+    mcmcsample(rng, model, sampler, nested_isdone; progressname="Nested Sampling", chain_type=Chains, kwargs...)
 
 StatsBase.sample(model, sampler::Nested; kwargs...) =
     StatsBase.sample(GLOBAL_RNG, model, sampler; kwargs...)
-
-function mcmcsample(
-    rng,
-    model,
-    sampler::Nested;
-    progress=true,
-    progressname="Nested Sampling",
-    discard_initial=0,
-    thinning=1,
-    chain_type=Any,
-    kwargs...
-)
-    # Obtain the initial sample and state.
-    sample, state = step(rng, model, sampler; kwargs...)
-
-    # Discard initial samples.
-    for _ in 2:discard_initial
-        # Obtain the next sample and state.
-        sample, state = step(rng, model, sampler, state; kwargs...)
-    end
-
-    # Save the sample.
-    samples = AbstractMCMC.samples(sample, model, sampler; kwargs...)
-    samples = save!!(samples, sample, 1, model, sampler; kwargs...)
-
-    # Step through the sampler until stopping.
-    i = 2
-        
-    @ifwithprogress progress name=progressname begin
-        while !nested_isdone(rng, model, sampler, samples, state, i; progress=progress, kwargs...)
-            # Discard thinned samples.
-            for _ in 1:(thinning - 1)
-                # Obtain the next sample and state.
-                _, state = step(rng, model, sampler, state; kwargs...)
-            end
-            # Obtain the next sample and state.
-            sample, state = step(rng, model, sampler, state; kwargs...)
-
-            # Save the sample.
-            samples = save!!(samples, sample, i, model, sampler; kwargs...)
-            # Increment iteration counter.
-            i += 1
-        end
-        logz_remain = maximum(state.logl) + state.logvol
-        delta_logz = logaddexp(state.logz, logz_remain) - state.logz
-    end
-
-    # Wrap the samples up.
-    return bundle_samples(samples, model, sampler, state, chain_type; kwargs...), state
-end
 
 function nested_isdone(rng, model, smapler, samples, state, i; progress=true, maxiter=Inf, maxcall=Inf, dlogz=0.5, maxlogl=Inf, kwargs...)
     # 1) iterations exceeds maxiter
@@ -72,8 +25,8 @@ function nested_isdone(rng, model, smapler, samples, state, i; progress=true, ma
     # TODO
 
     if progress
-        str = @sprintf "\33[2K\rsampling: iter=%d\tncall=%d\tΔlogz=%.2g\tlogl=%.2g\tlogz=%.2g" i state.ncall delta_logz state.logl_dead state.logz
-        print(str)
+        str = @sprintf "iter=%d\tncall=%d\tΔlogz=%.2g\tlogl=%.2g\tlogz=%.2g" i state.ncall delta_logz state.logl_dead state.logz
+        print("\r\33[2K", str)
     end
 
     return done_sampling
