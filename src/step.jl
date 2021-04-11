47fb7ffb349808c7277e2e90744d7e0f3bfd780a
Miles Lucas <mdlucas@hawaii.edu>
2021-04-11 14:02:52 -0500

    fix add_live_points and method dispatch bug

--- src/step.jl
+++ src/step.jl
@@ -31,10 +31,10 @@ function step(rng, model, sampler::Nested; kwargs...)
     h = logz
     logzvar = 2 * h * sampler.dlnvol
 
-    sample = (u=u_dead, v=v_dead, logwt=logwt, logl=logl_dead)
-    state = (it=1, ncall=ncall, us=us, vs=vs, logl=logl, logl_dead=logl_dead, logwt=logwt,
-             logz=logz, logzvar=logzvar, h=h, logvol=logvol,
-             since_update=since_update, has_bounds=false, active_bound=nothing)
+    sample = (u = u_dead, v = v_dead, logwt = logwt, logl = logl_dead)
+    state = (it = 1, ncall = ncall, us = us, vs = vs, logl = logl, logl_dead = logl_dead, logwt = logwt,
+             logz = logz, logzvar = logzvar, h = h, logvol = logvol,
+             since_update = since_update, has_bounds = false, active_bound = nothing)
 
     return sample, state
 end
@@ -45,13 +45,13 @@ function step(rng, model, sampler, state; kwargs...)
     # check if ready for first update
     if !state.has_bounds && state.ncall > sampler.min_ncall && state.it / state.ncall < sampler.min_eff
         @debug "First update: it=$(state.it), ncall=$(state.ncall), eff=$(state.it / state.ncall)"
-        active_bound = Bounds.scale!(Bounds.fit(sampler.bounds, state.us, pointvol = pointvol), sampler.enlarge)
+        active_bound = Bounds.scale!(Bounds.fit(sampler.bounds, state.us, pointvol=pointvol), sampler.enlarge)
         since_update = 0
         has_bounds = true
     # if accepted first update, is it time to update again?
     elseif iszero(state.since_update % sampler.update_interval)
         @debug "Updating bounds: it=$(state.it), ncall=$(state.ncall), eff=$(state.it / state.ncall)"
-        active_bound = Bounds.scale!(Bounds.fit(sampler.bounds, state.us, pointvol = pointvol), sampler.enlarge)
+        active_bound = Bounds.scale!(Bounds.fit(sampler.bounds, state.us, pointvol=pointvol), sampler.enlarge)
         since_update = 0
         has_bounds = true
     else
@@ -100,31 +100,28 @@ function step(rng, model, sampler, state; kwargs...)
     logzvar = state.logzvar + 2 * dh * sampler.dlnvol
 
     ## prepare returns
-    sample = (u=u_dead, v=v_dead, logwt=logwt, logl=logl_dead)
-    state = (it=state.it + 1, ncall=ncall, us=state.us, vs=state.vs, logl=state.logl, logl_dead=logl_dead, logwt=logwt,
-             logz=logz, logzvar=logzvar, h=h, logvol=logvol,
-             since_update=since_update, has_bounds=has_bounds, active_bound=active_bound)
+    sample = (u = u_dead, v = v_dead, logwt = logwt, logl = logl_dead)
+    state = (it = state.it + 1, ncall = ncall, us = state.us, vs = state.vs, logl = state.logl, logl_dead = logl_dead, logwt = logwt,
+             logz = logz, logzvar = logzvar, h = h, logvol = logvol,
+             since_update = since_update, has_bounds = has_bounds, active_bound = active_bound)
 
     return sample, state
 end
 
-function bundle_samples(samples, model, sampler::Nested, state, chain_type; add_live=true, kwargs...)
-    if add_live
-        samples, state = add_live_points(samples, model, sampler, state)
-    end
-    return bundle_samples(samples, model, sampler, state, chain_type; kwargs...)
-end
-
 function bundle_samples(samples,
-        ::AbstractModel,
+        model::AbstractModel,
         sampler::Nested,
         state,
         ::Type{Chains};
-        param_names = missing,
-        check_wsum = true,
+        add_live=true,
+        param_names=missing,
+        check_wsum=true,
         kwargs...)
 
-    vals = mapreduce(t->hcat(t.v..., t.logwt), vcat, samples)
+    if add_live
+        samples, state = add_live_points(samples, model, sampler, state)
+    end
+    vals = mapreduce(t -> hcat(t.v..., t.logwt), vcat, samples)
     # update weights based on evidence
     @. vals[:, end, 1] = exp(vals[:, end, 1] - state.logz)
     wsum = sum(vals[:, end, 1])
@@ -132,7 +129,7 @@ function bundle_samples(samples,
 
     if check_wsum
         err = !iszero(state.logzvar) ? 3 * sqrt(state.logzvar) : 1e-3
-        isapprox(wsum, 1, atol = err) || @warn "Weights sum to $wsum instead of 1; possible bug"
+        isapprox(wsum, 1, atol=err) || @warn "Weights sum to $wsum instead of 1; possible bug"
     end
 
     # Parameter names
@@ -141,32 +138,68 @@ function bundle_samples(samples,
     end
     push!(param_names, "weights")
 
-    return Chains(vals, param_names, Dict(:internals => ["weights"]), evidence = state.logz), state
+    return Chains(vals, param_names, Dict(:internals => ["weights"]), evidence=state.logz), state
 end
 
-bundle_samples(samples, ::AbstractMCMC.AbstractModel, ::Nested, ::Any, ::Type) = samples
-
 function bundle_samples(samples,
-        ::AbstractModel,
+        model::AbstractModel,
         sampler::Nested,
         state,
-        ::Type{A};
-        check_wsum = true,
-        kwargs...) where {A<:AbstractArray}
-
-    wsum = sum(samples) do samp
-        # update weights based on evidence
-        logwt = exp(samp.logwt - state.logz)
+        ::Type{Array};
+        add_live=true,
+        check_wsum=true,
+        kwargs...)
+    
+    if add_live
+        samples, state = add_live_points(samples, model, sampler, state)
     end
+
+    wsum = sum(s -> exp(s.logwt - state.logz), samples)
 
     if check_wsum
         err = !iszero(state.logzvar) ? 3 * sqrt(state.logzvar) : 1e-3
-        isapprox(wsum, 1, atol = err) || @warn "Weights sum to $wsum instead of 1; possible bug"
+        isapprox(wsum, 1, atol=err) || @warn "Weights sum to $wsum instead of 1; possible bug"
     end
 
-    vals = mapreduce(t->hcat(t.v..., t.logwt/wsum), vcat, samples)
+    vals = mapreduce(t -> hcat(t.v..., t.logwt / wsum), vcat, samples)
 
-    return vals
+    return vals, state
+end
+
+
+function bundle_samples(samples,
+    model::AbstractModel,
+    sampler::Nested,
+    state,
+    ::Type{Vector};
+    param_names=missing,
+    add_live=true,
+    check_wsum=true,
+    kwargs...)
+
+    if add_live
+        samples, state = add_live_points(samples, model, sampler, state)
+    end
+
+    wsum = sum(s -> exp(s.logwt - state.logz), samples)
+
+    if check_wsum
+        err = !iszero(state.logzvar) ? 3 * sqrt(state.logzvar) : 1e-3
+        isapprox(wsum, 1, atol=err) || @warn "Weights sum to $wsum instead of 1; possible bug"
+    end
+
+    # Parameter names
+    if param_names === missing
+        param_names = [Symbol("Parameter $i") for i in 1:sampler.ndims]
+    end
+    push!(param_names, :weights)
+
+    vals = map(samples) do sample
+        data = (sample.v..., sample.logwt / wsum)
+        NamedTuple(zip(param_names, data))
+    end
+
+    return vals, state
 end
 
 ## Helpers
@@ -183,7 +216,7 @@ function init_particles(rng, T, ndims, nactive, prior, loglike)
     us = rand(rng, T, ndims, nactive)
     vs = mapslices(prior, us, dims=1)
     logl = dropdims(mapslices(loglike, vs, dims=1), dims=1)
-    ntries = 1
+        ntries = 1
     while true
         any(isfinite, logl) && break
         us .= rand(rng, T, ndims, nactive)
@@ -214,43 +247,44 @@ function add_live_points(samples, model, sampler, state)
 
     dlnvol = -N / sampler.nactive
 
+    local logwt
+
     @inbounds for (i, idx) in enumerate(sorted_idxs)
         # get new point
         u = @view state.us[:, idx]
         v = @view state.vs[:, idx]
         ll = state.logl[idx]
 
-        logvol = log(1 - i / (sampler.nactive + 1))
+        logvol = prev_logvol + log(1 - i / (sampler.nactive + 1))
         logdvol = begin
             # weighted logaddexp
             X⁺ = max(prev_logvol, logvol)
-            X⁺ + log((exp(prev_logvol - X⁺) - exp(logvol - X⁺)) / 2)
+            X⁺ + log((exp(prev_logvol - X⁺) - exp(logvol - X⁺))) - log(2)
         end
         logwt = logaddexp(ll, prev_ll) + logdvol
-
-        logwt = logvol + logl
-
+        
         # update sampler
         logz = logaddexp(prev_logz, logwt)
-        h = (exp(logwt - logz) * logl +
+        lzterm = (exp(prev_ll - logz) * prev_ll +
+                  exp(ll - logz) * ll)
+        h = (exp(logdvol) * lzterm +
              exp(prev_logz - logz) * (prev_h + prev_logz) - logz)
         dh = h - prev_h
         logzvar = prev_logzvar + 2 * dh * dlnvol
 
-        prev_logvol = logvol
         prev_logz = logz
         prev_logzvar = logzvar
         prev_h = h
         prev_ll = ll
 
-        sample = (u=u, v=v, logwt=logwt, logl=ll)
-        save!!(samples, sample, N + i, model, sampler; kwargs...)
+        sample = (u = u, v = v, logwt = logwt, logl = ll)
+        save!!(samples, sample, N + i, model, sampler)
 
         i += 1
     end
 
-    state = (it=N + sampler.nactive, us=state.us, vs=state.vs, logl=state.logl, logl_dead=ll, logwt=logwt,
-            logz=prev_logz, logzvar=prev_logzvar, logvol=prev_logvol,
-            since_update=state.since_update, has_bounds=state.has_bounds, active_bound=state.active_bound)
+    state = (it = N + sampler.nactive, us = state.us, vs = state.vs, logl = state.logl, logl_dead = prev_ll, logwt = logwt,
+            logz = prev_logz, logzvar = prev_logzvar, logvol = prev_logvol,
+            since_update = state.since_update, has_bounds = state.has_bounds, active_bound = state.active_bound)
     return samples, state
 end
