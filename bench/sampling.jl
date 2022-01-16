8e82f7c42f929d460e76f2bc2be7ef297b08cc18
Miles Lucas <mdlucas@hawaii.edu>
2022-01-15 20:28:36 -1000

    add dynesty benchmark to set expectations

--- bench/sampling.jl
+++ bench/sampling.jl
@@ -3,15 +3,18 @@ using BenchmarkTools
 using CSV
 using NestedSamplers
 using ProgressLogging
+using PythonCall
 using StableRNGs
 using Statistics
 using StatsBase
 
+dy = pyimport("dynesty")
+
 rng = StableRNG(112358)
 
 rows = []
-dims = 2 .^ (1:5)
-@progress for D in dims
+dims = 2 .^ (1:4)
+@progress name="NestedSamplers.jl" for D in dims
     model, true_lnZ = Models.CorrelatedGaussian(D)
     splr = Nested(D, 50D; proposal=Proposals.Slice(slices=5), bounds=Bounds.Ellipsoid)
     # run once to extract values from state, also precompile
@@ -21,6 +24,24 @@ dims = 2 .^ (1:5)
     dlnZ = state.logz - true_lnZ
 
     row = (; library="NestedSamplers.jl", D, t=t, lnZ=state.logz, lnZstd=state.logzerr, dlnZ)
+    @info "$row"
+    push!(rows, row)
+end
+
+@progress name="dynesty" for D in dims[begin:end-1]
+    model, true_lnZ = Models.CorrelatedGaussian(D)
+    splr = dy.NestedSampler(
+        model.prior_transform_and_loglikelihood.loglikelihood,
+        model.prior_transform_and_loglikelihood.prior_transform,
+        D; nlive=50D, bound="single", sample="slice", slices=5
+    )
+    t = @elapsed splr.run_nested(dlogz=0.01)
+    res = splr.results
+    lnZ = PyArray(res["logz"], copy=false)[end]
+    lnZstd = PyArray(res["logzerr"], copy=false)[end]
+    dlnZ = lnZ - true_lnZ
+
+    row = (; library="dynesty", D, t, lnZ, lnZstd, dlnZ)
     @info "$row"
     push!(rows, row)
 end
